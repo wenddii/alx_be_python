9e9b793e7472ba654af9250e625494b6471d79a0
Wendwosen Tessema <wendwosen03@gmail.com>
2025-11-07 10:00:22 +0300

    Added simple_interest.py and fixed repo structure

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,11 @@
+# Store numbers
+number1 = 10
+number2 = 5
+
+sum_result = number1 + number2
+difference = number1 - number2
+product = number1 * number2
+
+print("The sum of", number1, "and", number2, "is", sum_result)
+print("The difference of", number1, "and", number2, "is", difference)
+print("The product of", number1, "and", number2, "is", product)
---
+++ python_introduction/simple_interest.py
@@ -0,0 +1,8 @@
+principal=1000
+rate =0.05
+time = 3
+
+Interest = principal*rate*time
+
+print(f"The simple interest is: {Interest}")
+
